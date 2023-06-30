23d66edac97cf99672e864a02fd8bed5a7607129
FelipeRibeir0 <fecribeiro2003@gmail.com>
2023-06-30 15:58:12 -0300

    add ex04

---
+++ secoes/sec01/ex04/index.php
@@ -0,0 +1,69 @@
+<?php 
+
+print "<h1>Operadores de atribuição</h1>";
+
+echo "<p>Concatenação: .=</p>";
+
+print "<h2>Atribuindo Strings</h2>";
+//Strings
+
+$name = "Felipe";
+
+$name .= " Ribeiro";
+
+echo "<p>".$name."</p>
+<hr>";
+
+//Numbers
+
+print "<h1>Operadores aritméticos</h1>";
+
+echo "<p>Adição: +=</p>";
+echo "<p>Subtração: -=</p>";
+echo "<p>Multiplicação: *=</p>";
+echo "<p>Divisão: /=</p>";
+echo "<p>Resto de divisão: %=</p>";
+echo "<p>Exponenciação: **=</p>";
+
+print "<h2>Atribuindo números</h2>";
+
+$add = 40;
+
+$add += 20;
+
+echo "<p> 40 + 20 = ".$add."</p>";
+
+$sub = $add;
+
+$sub -= 10;
+
+echo "<p> 60 - 10 = ".$sub."</p>";
+
+$multi = $sub;
+$multi *= 2;
+
+echo "<p> 50 x 2 = ".$multi."</p>";
+
+$div = $multi;
+$div /= 4;
+
+echo "<p> 100 / 4 = ".$div."</p>";
+
+$restoDiv = $div;
+
+$restoDiv %= 7;
+
+echo "<p> 25 % 7 = ".$restoDiv."</p>";
+
+$expo = $restoDiv;
+
+$expo **= 2;
+
+echo "<p> 4² = ".$expo."</p>
+<hr>";
+
+print "<h1>Operadores de incremento e decremento</h1>";
+
+echo "<p>Adição: ++</p>";
+echo "<p>Subtração: --</p>"
+?>
---
+++ secoes/sec01/ex04/readme.md
@@ -0,0 +1,7 @@
+# Anotações
+
+Resumi algumas aulas com esse index.php:
+
+A aula sobre Ponto e Vírgula (;), onde é ensinado que é necessário ter um ; após cada instrução, exceto na última.
+
+A aula sobre Operadores de atribuição, sobre Operadores Aritméticos e a atribuição deles, e por último os operadores de incremento e decremento, onde pode ser usado tanto o pré decremento quanto o pós incremento.
