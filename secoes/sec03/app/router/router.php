79a5a7563391b05c308171c7d5589c868e1bd6cc
FelipeRibeir0 <fecribeiro2003@gmail.com>
2023-08-09 23:24:48 -0300

    comecando a secao 03

---
+++ secoes/sec03/app/router/router.php
@@ -0,0 +1,62 @@
+<?php
+
+function routes()
+{
+    return require 'routes.php';
+}
+
+function exactMatchUri($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        return [$uri => $routes[$uri]];
+    }
+    return [];
+}
+
+function RegexMatchArrayRoutes($uri, $routes){
+    return array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+function params($uri,$matchedUri){
+    if(!empty($matchedUri)){
+        $matchedToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            explode('/', ltrim($uri, '/')),
+            explode('/', ltrim($matchedToGetParams, '/'))
+        );
+    }
+    return [];
+}
+
+function paramsFormat($uri, $params){
+    $uri = explode('/', ltrim($uri,'/'));
+    $paramsData = [];
+    foreach ($params as $index => $param) {
+        $paramsData[$uri[$index-1]] = $param;
+    }
+    return $paramsData;
+}
+
+function router()
+{
+    $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+    $routes = routes();
+
+    $matchedUri = exactMatchUri($uri, $routes);
+
+    if (empty($matchedUri)) {
+        $matchedUri = RegexMatchArrayRoutes($uri, $routes);
+        $params = params($uri,$matchedUri);
+        $params = paramsFormat($uri, $params);
+
+        var_dump($params);
+    }
+}
---
+++ secoes/sec03/app/router/routes.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+    '/' => 'Home@index',
+    '/user/create' => 'User@create',
+    '/user/[a-z0-9]+' => 'User@index',
+    '/user/[0-9]+/name/[a-zA-Z]+' => 'User@show'
+];
+
+?>
