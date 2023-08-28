78f2d68e45da545fe6ac3fa156e5f0bfa45f143f
FelipeRibeir0 <fecribeiro2003@gmail.com>
2023-08-28 16:37:17 -0300

    Implementando ENV

--- secoes/sec03/app/database/connect.php
+++ secoes/sec03/app/database/connect.php
@@ -2,9 +2,14 @@
 
 function connect()
 {
-    return new PDO("mysql:host=localhost;dbname=phpprofissional",'root','',[
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-    ]);
+    return new PDO(
+        "mysql:host={$_ENV['DATABASE_HOST']};dbname={$_ENV['DATABASE_NAME']}",
+        $_ENV['DATABASE_USER'],
+        $_ENV['DATABASE_PASSWORD'],
+        [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+        ]
+    );
 }
 
 
