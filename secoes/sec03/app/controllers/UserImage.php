fcadd706e19c83f087d9f25eacdc364fd3f314de
FelipeRibeir0 <fecribeiro2003@gmail.com>
2023-10-02 15:44:00 -0300

    add upload png image function

--- secoes/sec03/app/controllers/UserImage.php
+++ secoes/sec03/app/controllers/UserImage.php
@@ -6,8 +6,7 @@ namespace app\controllers;
 class UserImage
 {
     public function store(){
-        $extension = getExtension($_FILES['file']['name']);
-        isImage($extension);
+        upload();
     }
 }
 
--- secoes/sec03/app/helpers/image.php
+++ secoes/sec03/app/helpers/image.php
@@ -7,7 +7,7 @@ function getExtension($name)
 
 function isFileToUpload($fieldName)
 {
-    if ($_FILES[$fieldName] == null || $_FILES[$fieldName]['name'] == null || $_FILES[$fieldName]['name'] == '') {
+    if ($_FILES[$fieldName] == null || empty($_FILES[$fieldName]['name'])) {
         throw new Exception("O campo {$fieldName} não foi preenchido ou não foi escolhida uma imagem");
     }
 }
@@ -17,4 +17,16 @@ function isImage($extension)
     if (!in_array($extension, ['jpeg', 'jpg', 'gif', 'png'])) {
         throw new Exception("O arquivo não é aceito");
     }
+}
+
+function upload(){
+    $dst = imagecreatetruecolor(640,480);
+
+    [$width, $height] = getimagesize($_FILES['file']['tmp_name']);
+
+    $src = imagecreatefrompng($_FILES['file']['tmp_name']);
+
+    imagecopyresampled($dst,$src,0,0,0,0,640,480,$width,$height);
+
+    imagepng($dst,"assets/img/teste.png");
 }
